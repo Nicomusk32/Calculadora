4dcda84e10f4b946bf4cb01b2a53044091184ba2
Nicomusk <thenicotv13@gmail.com>
2025-03-30 20:59:36 -0500

    primer cambio

---
+++ index.js
@@ -0,0 +1,8 @@
+function añadir_producto(){
+var agregar_unproducto = document.getElementsByClassName("agregar_producto");
+if(agregar_unproducto == true){
+    alert("se añadio un producto");
+}
+
+
+}
